7a7a48ffac49505bfe6d42cc7e550fcd37e9c922
Juan Manuel Servera <jmservera@outlook.com>
2015-12-30 00:37:01 +0100

    add rgbled

--- RgbLed/MainPage.xaml.cs
+++ RgbLed/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,26 @@ namespace RgbLed
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            this.Unloaded += MainPage_Unloaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var pwmProvider = PwmSoftware.PwmProviderSoftware.GetPwmProvider();
+            var controller=(await Windows.Devices.Pwm.PwmController.GetControllersAsync(pwmProvider)).FirstOrDefault();
+            if(controller!= null)
+            {
+                var pinR = controller.OpenPin(5);
+                var pinG = controller.OpenPin(6);
+                var pinB = controller.OpenPin(7);
+                var led = new RgbLed(pinR, pinG, pinB);
+                led.On();
+                led.Color = Colors.LimeGreen;
+            }
+        }
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
---
+++ RgbLed/RgbLed.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Pwm;
+using Windows.UI;
+
+namespace RgbLed
+{
+    public class RgbLed : IDisposable
+    {
+        PwmPin _redPin, _greenPin, _bluePin;
+        PwmPin[] _pins;
+        public RgbLed(PwmPin redPin, PwmPin greenPin, PwmPin bluePin)
+        {
+            _redPin = redPin;
+            _greenPin = greenPin;
+            _bluePin = bluePin;
+            _pins = new[] { _redPin, _greenPin, _bluePin };
+        }
+
+        public void On()
+        {
+            for (int i = 0; i < _pins.Length; i++)
+            {
+                if (!_pins[i].IsStarted)
+                    _pins[i].Start();
+            }
+        }
+
+        public void Off()
+        {
+            for (int i = 0; i < _pins.Length; i++)
+            {
+                if (_pins[i].IsStarted)
+                    _pins[i].Stop();
+            }
+        }
+
+        private Color _color;
+
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                updateLed();
+            }
+        }
+
+
+        private void updateLed()
+        {
+            double[] colorValue = new double[] { Color.R, Color.G, Color.B };
+            for (int i = 0; i < colorValue.Length; i++)
+            {
+                colorValue[i] /= 255d;
+                _pins[i].SetActiveDutyCyclePercentage(colorValue[i]);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_pins != null)
+            {
+                for (int i = 0; i < _pins.Length; i++)
+                {
+                    if (_pins[i].IsStarted)
+                    {
+                        _pins[i].Stop();
+                    }
+                    _pins[i].Dispose();
+                }
+                _pins = null;
+            }
+        }
+    }
+}
